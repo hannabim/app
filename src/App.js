252b6c35de9edec9a32f6ef3866479651c05d4a2
olha <126962949+hannabim@users.noreply.github.com>
2023-05-28 18:09:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import SearchEngine from "./SearchEngine";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <SearchEngine />
-  <a href="https://github.com/hannabim/app">Open-sourced code </a>
-  </StrictMode>
-);
+export default function App() {
+  root.render(
+    <StrictMode>
+      <SearchEngine />
+      <a href="https://github.com/hannabim/app">Open-sourced code </a>
+    </StrictMode>
+  );
+}

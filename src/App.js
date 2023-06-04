4a65428e3477e9babdc0c4fef66f01d30bf7d309
olha <126962949+hannabim@users.noreply.github.com>
2023-06-04 14:29:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import "./App.css";
 import Weather from "./Weather";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);

4600b6a94ef9487e4009ff4631301843c20df36f
olha <126962949+hannabim@users.noreply.github.com>
2023-06-04 14:39:58 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,8 @@ export default function Weather() {
               src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
               alt="mostly cloudy"
             />
-            6°C
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
           </div>
           <div className="col-6">
             <ul>
@@ -29,4 +30,3 @@ export default function Weather() {
       </ul>
     </div>
   );
-}

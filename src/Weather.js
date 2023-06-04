e0c6f84772213e7bf3c2a9e3cdebe023d069babe
olha <126962949+hannabim@users.noreply.github.com>
2023-06-04 13:08:59 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,1 +1,30 @@
+import React from "react";
+import "./Weather.css";
 
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Lisbon</h1>
+      <ul>
+        <li>Wednesday 17:00</li>
+        <li>Mostly cloudy</li>
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="mostly cloudy"
+            />
+            6°C
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 12 km/h</li>
+            </ul>
+          </div>
+        </div>
+      </ul>
+    </div>
+  );
+}

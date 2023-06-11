52947c7d1b2b7462e2452d3844ad2d4b38a4fcd4
olha <126962949+hannabim@users.noreply.github.com>
2023-06-11 15:03:06 +0300

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -3,15 +3,7 @@ export default function WeatherForecastDay(props) {
   function day() {
     let date = new Date(props.data.time * 1000);
     let day = date.getDay();
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Sunday",
-    ];
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sun"];
     return days[day];
   }
 

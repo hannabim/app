bf197216a6c6a057854161bfcb2a0f99ef086d80
olha <126962949+hannabim@users.noreply.github.com>
2023-06-17 09:18:12 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -9,57 +9,5 @@ export default function Weather(props){
    useEffect(() => {
   setLoaded(false);
  }, [props.city]);
-  let form = (
-    <div className="Search">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city..."
-              autoFocus="on"
-              onChange={ChangeCity}
-            />
-          </div>
-          <div className="col-3">
-            {" "}
-            <button type="submit" className="btn btn-primary">
-              {" "}
-              Search{" "}
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
-  if (loaded) {
-    return (
-      <div className="Weather">
-        {form}
-        <h1 className="text-capitalize">{city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weather.date} />
-          </li>
-          <li className="text-capitalize description">{weather.description}</li>
-          <div className="row">
-            <div className="col-6">
-              <img src={weather.icon} alt="current weather icon" />
-              <WeatherTemperature celsius={Math.round(weather.temperature)} />
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>Humidity: {weather.humidity}%</li>
-                <li>Wind: {Math.round(weather.wind)} km/h</li>
-              </ul>
-            </div>
-          </div>
-        </ul>
-        <WeatherForecast city={city} />
-      </div>
-    );
-  } else {
-    return form;
-  }
-}
+
 }

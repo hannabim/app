7490cf22fc4da66c7a16231eef3e64137d5f6405
olha <126962949+hannabim@users.noreply.github.com>
2023-06-17 09:38:15 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
-import React, {  useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Weather.css";
 import WeatherForecast from "./WeatherForecast";
 import FormattedDate from "./FormattedDate";
 import SearchEngine from "./Searchengine"
 import WeatherTemperature from "./WeatherTemperature";
-
+ const [loaded, setLoaded] = useState(false);
 export default function Weather(props){
    useEffect(() => {
   setLoaded(false);

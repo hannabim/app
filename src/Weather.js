a7ce70d74feb38eadbb1787872fe082d4364b40b
olha <126962949+hannabim@users.noreply.github.com>
2023-06-17 08:46:54 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -10,7 +10,7 @@ export default function SearchEngine() {
   let [weather, setWeather] = useState({});
   useEffect(() => {
    setLoaded(false);
-  }, [onSubmit]);
+  }, [city]);
   function ChangeCity(event) {
     setCity(event.target.value);
   }

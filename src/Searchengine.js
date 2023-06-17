0b058c54f23e52e79e922ba777ca42ed9e08c15e
olha <126962949+hannabim@users.noreply.github.com>
2023-06-17 09:02:41 +0300

    Update Searchengine.js

--- src/Searchengine.js
+++ src/Searchengine.js
@@ -1,1 +1,29 @@
+import React{UseState, UseEffect} from react;
+export default function SearchEngine() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+ useEffect(() => {
+  setLoaded(false);
+ }, []);
+  function ChangeCity(event) {
+    setCity(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "599abtb07236d08d64676o3f61450582";
+    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(url).then(displayWeather);
+  }
 
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      date: new Date(response.data.time * 1000),
+      description: response.data.condition.description,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      icon: response.data.condition.icon_url,
+    });
+  }

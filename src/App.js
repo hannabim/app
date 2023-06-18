2c9fc23983908ff1a524c10bca1570de326b5b1c
olha <126962949+hannabim@users.noreply.github.com>
2023-06-18 10:27:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./App.css";
-import SearchEngine from "./Searchengine";
 
 import Weather from "./Weather";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 export default function App() {
+  let defaultcity = "Kiev";
   root.render(
     <div className="App">
       <div className="container">
         <StrictMode>
-          <SearchEngine />
+          <Weather city={defaultcity} />
           <footer>
             <a
               href="https://github.com/hannabim/app"
@@ -26,4 +26,3 @@ export default function App() {
     </div>
   );
 }
-

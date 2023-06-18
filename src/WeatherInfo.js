bc3ac89394292e623b45f65c21a49a8ed73d0e76
olha <126962949+hannabim@users.noreply.github.com>
2023-06-18 11:26:54 +0300

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,26 +1,47 @@
 import React from "react";
-import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Sunday",
+  ];
+  let day = props.weather.date.getDay();
+  let hours = props.weather.date.getHours();
+  let minutes = props.weather.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   return (
     <div className="WeatherInfo">
-      <h1 className="text-capitalize">{props.info.city}</h1>
+      <h1 className="text-capitalize">{props.weather.city}</h1>
       <ul>
         <li>
-          <FormattedDate date={props.info.date} />
+          <div>
+            {days[day]}, {hours}:{minutes}{" "}
+          </div>
         </li>
         <li className="text-capitalize description">
-          {props.info.description}
+          {props.weather.description}
         </li>
         <div className="row">
           <div className="col-6">
-            <img src={props.info.icon} alt="current weather icon" />
-            <WeatherTemperature celsius={Math.round(props.info.temperature)} />
+            <img src={props.weather.icon} alt="current weather icon" />
+            <WeatherTemperature
+              celsius={Math.round(props.weather.temperature)}
+            />
           </div>
           <div className="col-6">
             <ul>
-              <li>Humidity: {props.info.humidity}%</li>
-              <li>Wind: {Math.round(props.info.wind)} km/h</li>
+              <li>Humidity: {props.weather.humidity}%</li>
+              <li>Wind: {Math.round(props.weather.wind)} km/h</li>
             </ul>
           </div>
         </div>
